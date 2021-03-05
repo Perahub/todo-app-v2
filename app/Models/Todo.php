e80a5ed87703075484e4af7224023bfbdcb1af9c
seikiyoshin <bandjosilvano@gmail.com>
2021-03-05 10:16:13 +0800

    todos

---
+++ app/Models/Todo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $table = 'todos';
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_finished' => 'boolean',
+    ];
+}
