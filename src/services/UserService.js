d5387a5c045e8fe67d03de174171231ce52d7c9c
ArcIX <rc2582739@gmail.com>
2021-03-27 20:21:05 +0800

    Remove unneeded functions

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -11,38 +11,6 @@ class UserService {
             return null;
         }
     }
-
-    static async addPassword(id, password) {
-        console.log(id, password)
-        const requestOptions = {
-            method: 'PATCH',
-            body: JSON.stringify({
-                password: password
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        }
-
-        const response = await fetch(url + `/${id}`, requestOptions)
-        const user = await response.json();
-        console.log(user)
-    }
-
-    static async update(user) {
-        const requestOptions = {
-            method: 'PUT',
-            body: JSON.stringify({
-                title: user.title,
-                completed: user.completed
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        }
-
-        return requestOptions;
-    }
 }
 
 export default UserService;
